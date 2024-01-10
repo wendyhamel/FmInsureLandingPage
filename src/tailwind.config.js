ba775fb1e30915c2513240f104dd8ee8de610720
wendyhamel <whamel@it4web.nl>
2024-01-10 12:52:10 +0100

    Page structure

---
+++ src/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+	theme: {
+		fontFamily: {
+			'sans': ['Karla', 'sans-serif'],
+			'heading': ['DM Serif Display', 'serif'],
+		},
+		extend: {
+			colors: {
+				'blue': 'hsl(216, 30%, 68%)',
+				'violet': 'hsl(256, 26%, 20%)',
+				'dark-violet': 'hsl(270, 9%, 17%)',
+				'gray-violet': 'hsl(273, 4%, 51%)',
+				'light-gray': 'hsl(0, 0%, 98%)',
+			},
+		},
+	}
+}
